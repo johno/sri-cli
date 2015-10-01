d885ed3bcbcd3809329f2a1c3d0079e15cedccd0
John Otander <johnotander@gmail.com>
2015-10-01 14:13:40 -0600

    Add useful tests

--- test/test.js
+++ test/test.js
@@ -1,10 +1,27 @@
 'use strict'
 
+var fs = require('fs')
 var test = require('tape')
-var sriCli = require('..')
+var pathExists = require('path-exists')
+var spawn = require('child_process').spawn
 
 test('sri-cli', function (t) {
   t.plan(1)
 
-  t.equal(sriCli(), false)
+  var cp = spawn('./cli.js', ['test/test.js'])
+
+  cp.on('close', function () {
+    t.assert(true)
+  })
+})
+
+test('sri-cli with --json-out', function (t) {
+  t.plan(1)
+
+  var cp = spawn('./cli.js', ['test/test.js', '--json-out=tmp.json'])
+
+  cp.on('close', function () {
+    t.assert(pathExists.sync('tmp.json'))
+    fs.unlink('tmp.json')
+  })
 })
